9fcc86d4dd1b0664bdb9efb3f3d0c54a6d61ac1a
Guido Percú <guidopercu@gmail.com>
2019-02-16 23:16:57 -0300

    Fazendo curso de VueJS - desafio1

---
+++ 2-dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Guido',
+        idade: 25,
+        imagem: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    }
+});
