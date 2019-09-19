d6cf0f11794af1296c327def601ec42b57492839
Guido Percú <guidopercu@gmail.com>
2019-09-19 20:02:53 -0300

    App ReactJS simples

---
+++ react-redux-pt/todoapp/frontend/src/main/app.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import Todo from '../todo/todo'
+
+export default props => (
+    <div className='container'>
+        <Todo />
+    </div>
+)
---
+++ react-redux-pt/todoapp/frontend/src/todo/todo.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        <div className="container">
+            <h1>Teste aaaaaaaaaaa</h1>
+        </div>
+    }
+}
